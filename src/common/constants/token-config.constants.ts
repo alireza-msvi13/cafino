9c11b2f094a8238613740b2e89de7e5de7ad408a
Alireza-edk13 <143755210+Alireza-edk13@users.noreply.github.com>
2025-07-07 15:49:28 +0330

    fix: cookie

--- src/common/constants/token-config.constants.ts
+++ src/common/constants/token-config.constants.ts
@@ -3,13 +3,13 @@ import { CookieOptions } from "express";
 export const AccessCookieConfig: CookieOptions = {
     sameSite: 'none',
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'development',
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 1 * 24 * 60 * 60 * 1000,
 };
 
 export const RefreshCookieConfig: CookieOptions = {
     sameSite: 'none',
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'development',
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
